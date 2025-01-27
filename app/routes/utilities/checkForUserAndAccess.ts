feb6a7658304836b814e3482972751aa5232cea0
Ashutosh Palania <ashutosh.palania@dream11.com>
2025-01-27 16:25:25 +0530

    refactor

--- app/routes/utilities/checkForUserAndAccess.ts
+++ app/routes/utilities/checkForUserAndAccess.ts
@@ -9,21 +9,12 @@ export const getUserAndCheckAccess = async (param: {
 }) => {
   const user = await getUser(param.request)
 
-  console.log('==>>User:', user)
-
   const isAllowed = await isUserAllowedToAccess({
     resource: param.resource.replace(/^api\/v[0-9]+\//, ''),
     action: ApiToTypeMap[param.resource],
     userId: user?.userId ?? 0,
     role: user?.role,
   })
-
-  console.log(
-    '==>>isAllowed:',
-    isAllowed,
-    param.resource,
-    ApiToTypeMap[param.resource],
-  )
 
   if (!isAllowed) {
     throw new Error(ACCESS_ERROR_MESSAGE)
